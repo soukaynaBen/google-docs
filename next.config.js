6e56cbaa2a3229168acbe53e3d8cf59ddd638117
soukayna ben <soukabenlahbib@gmail.com>
2022-03-04 16:25:22 +0100

    disable optimizeFonts

--- next.config.js
+++ next.config.js
@@ -3,7 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   images:{
     domains:["links.papareact.com","lh3.googleusercontent.com"],
-  }
+  },
+  optimizeFonts: false,
+
 }
 
 module.exports = nextConfig
